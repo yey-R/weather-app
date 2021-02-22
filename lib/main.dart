957b4a56edf990041271a57323fd56e18ba13864
Emre YAZICI <yee.yazici@gmail.com>
2021-02-22 13:51:03 +0300

    Blank project

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
